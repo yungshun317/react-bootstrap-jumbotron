47306c77425536c451cc57ae38cb800d80297340
yungshun317 <seraph317@gmail.com>
2021-05-31 20:16:49 +0800

    [FEAT] Push notifications with a `Toast` component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from "react";
 
 import Container from "react-bootstrap/Container";
 import Jumbotron from "react-bootstrap/Jumbotron";
+import Toast from "react-bootstrap/Toast";
 
 const App = () => (
 	<Container className="p-3">
 	    <Jumbotron>
 	        <h1 className="header">Welcome to React-Bootstrap Jumbotron</h1>
+	        <Toast>
+	            <Toast.Header>
+	                <strong className="mr-auto">Bootstrap</strong>
+	                <small>4 mins ago</small>
+	            </Toast.Header>
+	            <Toast.Body>Hello world! This is a toast message.</Toast.Body>
+	        </Toast>
 	    </Jumbotron>
 	</Container>
 );
