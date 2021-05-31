7a4b097475709a574d7dc5a518729490147fe8ca
yungshun317 <seraph317@gmail.com>
2021-05-31 19:47:37 +0800

    [FEAT] Create a `Jumbotron` component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 import Container from "react-bootstrap/Container";
+import Jumbotron from "react-bootstrap/Jumbotron";
 
 const App = () => (
 	<Container className="p-3">
+	    <Jumbotron>
+	        <h1 className="header">Welcome to React-Bootstrap Jumbotron</h1>
+	    </Jumbotron>
 	</Container>
 );
 
