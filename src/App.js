1c6ce1a4e44effaac69155f90db3d5397cebb1dc
yungshun317 <seraph317@gmail.com>
2021-06-03 08:27:07 +0800

    [FEAT] Add a toggle button

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Toast from "react-bootstrap/Toast";
+import Button from "react-bootstrap/Button";
 
 const CustomizedToast = ({ children }) => {
 	const [show, toggleShow] = useState(true);
 
 	return (
 		<>
-		    // {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
+		    {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
 		    <Toast show={show} onClose={() => toggleShow(false)}>
 		        <Toast.Header>
 		            <strong className="mr-auto">Bootstrap</strong>
 	                <small>4 mins ago</small>
 		        </Toast.Header>
-		        <Toast.Body>Hello world! This is a toast message.</Toast.Body>
+		        <Toast.Body>{children}</Toast.Body>
 		    </Toast>
 		</>
 	)
@@ -26,6 +27,10 @@ const App = () => (
 	    <Jumbotron>
 	        <h1 className="header">Welcome to React-Bootstrap Jumbotron</h1>
 	        <CustomizedToast>
+	            Hello world! This is a toast message.
+	            <span role="img" aria-label="tada">
+	                🎉
+	            </span>
 	        </CustomizedToast>
 	    </Jumbotron>
 	</Container>
